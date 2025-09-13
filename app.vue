f9bccb8cc403dec006ca0c39a237c2f3ff4b4c3d
Romin Manogil <romin@valuemycar.fr>
2025-09-13 16:56:27 +0200

    style: added grab wrapper

--- app.vue
+++ app.vue
@@ -1,7 +1,17 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+    <div >
+      <resize-wrapper>
+        <template #left>
+          <user-library />
+        </template>
+
+        <template #right>
+          <NuxtPage />
+        </template>
+      </resize-wrapper>
+    </div>
+    </NuxtLayout>
 </template>
 
 <script setup lang='ts'>
---
+++ components/global/main-wrapper.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='main-wrapper'>
+      <slot />
+    </div>
+</template>
+
+<script setup lang='ts'>
+    
+</script>
+
+<style lang='scss' scoped>
+    .main-wrapper{
+      border: solid 1px;
+    }
+</style>
