fd9728d22e3a73b87779cb8fbf3d7ece13bb1c1a
RominM <rominmanogil.off@gmail.com>
2025-09-13 17:53:58 +0200

    style: added button to left menu

--- components/global/search-module/search-module.vue
+++ components/global/search-module/search-module.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class='search-module'></div>
+    <div class='search-module'>
+      <h-icon :icon="Search01Icon" color="#b3b3b3"/>
+      <input class='search-module__input' type="text" placeholder="Que qouhaitez-vous écouter ou regarder ?"/>
+      <h-icon :icon="DeliveryBox01Icon" color="#b3b3b3"/>
+    </div>
 </template>
 
 <script setup lang='ts'>
+import { DeliveryBox01Icon, Search01Icon } from '@hugeicons/core-free-icons';
+
     
 </script>
 
 <style lang='scss' scoped>
     .search-module{
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      padding: 10px;
       width: 100%;
-      height: 100%;
+      height: stretch;
+      border-radius: 24px;
       background-color: $dark-surface;
+      &__input {
+        width: 100%;
+        border-right: solid 1px $light-text-secondary;
+        &::placeholder {
+          color: $light-text-secondary;
+        }
+      }
     }
 </style>
