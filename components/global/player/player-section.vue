4ac1961195383871bd2530919cb27b66765675b4
Romin Manogil <romin@valuemycar.fr>
2025-09-14 10:36:31 +0200

    style: init player

---
+++ components/global/player/player-section.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class='player-section'>
+    <on-listen />
+    <player />
+    <player-menu />
+  </section>
+</template>
+
+<script setup lang='ts'>
+    
+</script>
+
+<style lang='scss' scoped>
+  .player-section{
+    display: flex;
+    height: 100px;
+    padding: 10px;
+    background-color: $dark-background;
+    & > * {
+      width: calc(100% / 3);
+    }
+  }
+</style>
