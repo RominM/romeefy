e2946ba4f1b80b93a8f28c70a4ccf990ff985f98
Romin Manogil <romin@valuemycar.fr>
2025-09-15 00:50:29 +0200

    style: try animation to scroll artist

--- components/global/player/on-listen.vue
+++ components/global/player/on-listen.vue
@@ -1,54 +1,104 @@
 <template>
-  <div class='on-listen'>
-    <img v-if="track" class='on-listen__cover' :src="track.album.cover_small" alt="">
-    <div class='on-listen__artist'>
-      <p v-if="track">{{ track.title }}</p>
-      <p v-if="track">{{ track.artist.name }}</p>
+  <div class="on-listen">
+    <img v-if="track" class="on-listen__cover" :src="track.album.cover_small" alt="" />
+    <div class="on-listen__artist">
+      <p
+        ref="titleRef"
+        :style="scrollStyles.title"
+      >
+        {{ track?.title }}
+      </p>
+      <p
+        ref="artistRef"
+        :style="scrollStyles.artist"
+      >
+        {{ track?.artist.name }}
+      </p>
     </div>
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import { ref, reactive, nextTick } from 'vue';
 import { EGlobalEvent } from '~/types/enum/global/globalEvent';
 
-useGlobalEvents().subscribeTo(EGlobalEvent.TRACK_DATA, (payload) => {
-  console.log({payload});
-  track.value = payload
-  
-})
+const track = ref<any>();
 
-const track = ref()
+const titleRef = ref<HTMLElement | null>(null);
+const artistRef = ref<HTMLElement | null>(null);
+
+const scrollStyles = reactive<{ title: any; artist: any }>({ title: {}, artist: {} });
+
+useGlobalEvents().subscribeTo(EGlobalEvent.TRACK_DATA, (payload) => {
+  track.value = payload;
+
+  nextTick(() => {
+    updateScroll(titleRef.value, scrollStyles, 'title');
+    updateScroll(artistRef.value, scrollStyles, 'artist');
+  });
+});
+
+function updateScroll(el: HTMLElement | null, styleObj: any, key: 'title' | 'artist') {
+  if (!el) return;
+  const parentWidth = el.parentElement?.offsetWidth || 0;
+  const textWidth = el.scrollWidth;
+
+  if (textWidth > parentWidth) {
+    const distance = textWidth - parentWidth;
+    const duration = distance / 30; // ajustable: plus grand = plus lent
+    styleObj[key] = {
+      display: 'inline-block',
+      whiteSpace: 'nowrap',
+      animation: `scrollPingPong ${duration}s linear infinite alternate`
+    };
+    el.style.setProperty('--scroll-distance', `${distance}px`);
+  } else {
+    styleObj[key] = { whiteSpace: 'nowrap', display: 'inline-block' };
+  }
+}
 </script>
 
-<style lang='scss' scoped>
-  .on-listen{
+<style scoped lang="scss">
+.on-listen {
+  display: flex;
+  gap: 10px;
+  color: #fff;
+  width: 100%;
+
+  &__cover {
+    width: 72px;
+    height: 72px;
+    object-fit: cover;
+  }
+
+  &__artist {
     display: flex;
-    gap: 10px;
-    color: #fff;
-    &__cover {
-      width: 72px;
-      height: 72px;
-      object-fit: cover;
+    flex-direction: column;
+    justify-content: center;
+    gap: 5px;
+    width: 100%;
+    overflow: hidden;
+
+    :first-child {
+      font-size: 14px;
+      font-weight: 600;
+      color: $light-background;
     }
-    &__artist {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      gap: 5px;
-      & > * {
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-      }
-      :first-child {
-        font-size: 14px;
-        font-weight: 600;
-        color: $light-background;
-      }
-      :nth-child(2) {
-        font-size: 12px;
-        color: $light-text-primary;
-      }
+
+    :nth-child(2) {
+      font-size: 12px;
+      color: $light-text-primary;
     }
   }
+}
+
+// Animation ping-pong dynamique
+@keyframes scrollPingPong {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(var(--scroll-distance) * -1));
+  }
+}
 </style>
