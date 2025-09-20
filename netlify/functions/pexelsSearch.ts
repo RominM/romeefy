e879428dbecd09fd5f356fec3c032f8d8c02f097
Romin Manogil <romin@valuemycar.fr>
2025-09-21 00:05:19 +0200

    chore: setup netlify

---
+++ netlify/functions/pexelsSearch.ts
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+
+export const handler = async (event) => {
+  const query = event.queryStringParameters?.q || 'nature'
+  const perPage = event.queryStringParameters?.per_page || 10
+  const apiKey = process.env.PEXELS_API_KEY
+
+  if (!apiKey) {
+    return { statusCode: 500, body: JSON.stringify({ error: true, message: 'API key missing' }) }
+  }
+
+  const res = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=${perPage}`, {
+    headers: { Authorization: apiKey }
+  })
+
+  const data = await res.json()
+  return { statusCode: 200, body: JSON.stringify(data) }
+}
