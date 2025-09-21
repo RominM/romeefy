a572da74224bd063d01b4b7df75eeb4d1c43d296
Romin Manogil <romin@valuemycar.fr>
2025-09-21 03:22:57 +0200

    style: in progress

--- components/artist/discography/discography-header.vue
+++ components/artist/discography/discography-header.vue
@@ -1,14 +1,18 @@
 <template>
   <header class="discography-header">
+    <pre>{{ currentAlbum }}</pre>
     <player-top-fixed :name="artistName" color="#100f0f" :scroll-top="targetScroll" :track-id="trackId"/>
   </header>
 </template>
 
 <script setup lang="ts">
+import type { PropType } from 'vue';
+
 defineProps({
   artistName: { type: String, required: true },
   targetScroll: { type: Number, required: true },
   trackId: { type: Number, required: true },
+  currentAlbum: { type: Object as PropType<IAlbum>, required: true}
 })
 </script>
 
