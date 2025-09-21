c8cc637eb69acbefc694a32347aa49448d0807fe
RominM <rominmanogil.off@gmail.com>
2025-09-21 22:47:19 +0200

    dev: init page

--- composables/formats/useFormatTime.ts
+++ composables/formats/useFormatTime.ts
@@ -1,5 +1,5 @@
-export const useFormatTime = (time: number) => {
+export const useFormatTime = (time: number, pretty = false) => {
   const minutes = Math.floor(time / 60)
   const seconds = Math.floor(time % 60).toString().padStart(2, "0")
-  return `${minutes}:${seconds}`
+  return pretty ? `${minutes} m ${seconds} s` : `${minutes}:${seconds}`
 }
