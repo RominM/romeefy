865146ee95d9338708e8a0841cc21879be5d1f1c
RominM <rominmanogil.off@gmail.com>
2025-10-18 15:00:53 +0200

    dev: added modal & init playlist modal component

---
+++ components/playlist/create-playlist.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="create-playlist">
+    <p>CREATE PLAYLIST</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.create-playlist {
+  
+}
+</style>
