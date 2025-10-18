79a0c71116f7e6da343d81cf409583f7b60ede10
Romin Manogil <romin@valuemycar.fr>
2025-10-18 17:00:17 +0200

    dev: handle create playlit dropdown

--- components/global/user-library/user-library-header.vue
+++ components/global/user-library/user-library-header.vue
@@ -5,29 +5,16 @@
       <h2 class="user-library-header__dynamic-title__title">Bibliothèque</h2>
     </div>
 
-    <div style="display: flex; gap: 10px;">
-      <button class="user-library-header__create" title="Gérez un dossier, une playlist ou un Jam">
-        <h-icon :icon="Add01Icon" size="18"/>
-        Créer
-      </button>
-      <button 
-        class="user-library-header__expand-shrink" 
-        :title="`${isExpanded ? 'Réduire' : 'Agrandir'} la Bibliothèque`" 
-      >
-        <h-icon v-if="!isExpanded" :icon="ArrowExpand01Icon" size="18"/>
-        <h-icon v-else :icon="ArrowShrink02Icon" size="18"/>
-      </button>
-    </div>
+    <handling-top-library />
   </header>
 </template>
 
 <script setup lang="ts">
-import { Add01Icon, ArrowExpand01Icon, ArrowShrink02Icon, BarCode02Icon } from '@hugeicons/core-free-icons';
+import { BarCode02Icon } from '@hugeicons/core-free-icons';
 const props = defineProps({
   hovered: { type: Boolean, default: false }
 })
 
-const isExpanded = ref<boolean>(false)
 </script>
 
 <style scoped lang="scss">
@@ -55,27 +42,6 @@ const isExpanded = ref<boolean>(false)
         opacity: 1;
       }
     }
-  }
-
-  &__create,
-  &__expand-shrink {
-    display: flex;
-    gap: 5px;
-    transition: 0.3s;
-    border-radius: 25px;
-    &:hover {
-      background-color: $dark-surface-secondary;
-    }
-  }
-
-  &__create {
-    padding: 8px 12px;
-    background-color: $dark-surface;
-    font-size: 14px;
-  }
-  &__expand-shrink {
-    padding: 5px 10px;
-
   }
 }
 </style>
