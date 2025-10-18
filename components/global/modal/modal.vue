fd2099cd7e1a7b82987c54e1c33c102cbe692fb9
RominM <rominmanogil.off@gmail.com>
2025-10-18 15:07:23 +0200

    style: added effect on modal

--- components/global/modal/modal.vue
+++ components/global/modal/modal.vue
@@ -1,26 +1,31 @@
 <template>
-  <div v-if="isOpen" class="modal" @click="close">
-    <div 
-      class="modal__dialog"
-      :style="{ minWidth: minWidth, maxWidth: maxWidth}"
-      @click.stop
-    >
-      <div class="modal__dialog--header">
-        <h4>{{ title }}</h4>
-        <h-icon 
-          v-if="dismisable && showCross" 
-          class="modal__dialog--header--cross" 
-          :icon="MultiplicationSignIcon"
-          @click="close"  
-        />
-      </div>
-      <slot />
+  <transition name="modal-fade">
+    <div v-if="isOpen" class="modal" @click="close">
+      <transition name="dialog-zoom">
+        <div 
+          v-if="isOpen" 
+          class="modal__dialog"
+          :style="{ minWidth: minWidth, maxWidth: maxWidth }"
+          @click.stop
+        >
+          <div class="modal__dialog--header">
+            <h4>{{ title }}</h4>
+            <h-icon 
+              v-if="dismisable && showCross" 
+              class="modal__dialog--header--cross" 
+              :icon="MultiplicationSignIcon"
+              @click="close"  
+            />
+          </div>
+          <slot />
+        </div>
+      </transition>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script setup lang="ts">
-import { MultiplicationSignIcon } from '@hugeicons/core-free-icons';
+import { MultiplicationSignIcon } from '@hugeicons/core-free-icons'
 
 const props = defineProps({
   isOpen: { type: Boolean, default: false },
@@ -34,7 +39,7 @@ const props = defineProps({
 const emit = defineEmits(['update:isOpen'])
 
 function close() {
-  if(!props.dismisable) return
+  if (!props.dismisable) return
   emit('update:isOpen', false)
 }
 </script>
@@ -46,26 +51,47 @@ function close() {
   left: 0;
   width: 100dvw;
   height: 100dvh;
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 999;
-  &__dialog {
-    min-width: 50px;
-    min-height: 50px;
-    padding: 10px;
-    border-radius: 8px;
-    background-color: $dark-surface;
-    &--header {
-      display: flex;
-      justify-content: space-between;
-      gap: 10px;
-      color: #fff;
-      &--cross {
-        margin-left: auto;
-      }
+}
+
+.modal__dialog {
+  min-width: 50px;
+  min-height: 50px;
+  padding: 10px;
+  border-radius: 8px;
+  background-color: $dark-surface;
+  transform-origin: center;
+  &--header {
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    color: #fff;
+    &--cross {
+      margin-left: auto;
     }
   }
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+.modal-fade-enter-from,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.dialog-zoom-enter-active,
+.dialog-zoom-leave-active {
+  transition: all 0.25s ease;
+}
+.dialog-zoom-enter-from,
+.dialog-zoom-leave-to {
+  transform: scale(0.9);
+  opacity: 0;
 }
 </style>
