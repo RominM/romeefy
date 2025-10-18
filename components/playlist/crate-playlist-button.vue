e20807a8882dc3580fa0e73a09c3da5a7e578560
Romin Manogil <romin@valuemycar.fr>
2025-10-18 15:47:22 +0200

    dev: init playlist module

---
+++ components/playlist/crate-playlist-button.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="crate-playlist-button">
+    <div class="crate-playlist-button__wrapper-icon">
+      <h-icon :icon="playlistButton.icon" size="22" />
+    </div>
+
+    <div>
+      <p class="crate-playlist-button__title">{{ playlistButton.title }}</p>
+      <p class="crate-playlist-button__subtitle">{{ playlistButton.subtitle }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+defineProps({
+  playlistButton: { type: Object as PropType<TPlaylistButton>, required: true} 
+})
+</script>
+
+<style scoped lang="scss">
+.crate-playlist-button {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: #fff;
+  &__wrapper-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    background-color: $dark-surface-secondary;
+    border-radius: 50%;
+  }
+
+  &__title {
+    font-size: 14px;
+    font-weight: 600;
+  }
+
+  &__subtitle {
+    font-size: 12px;
+    font-weight: 300;
+  }
+}
+</style>
