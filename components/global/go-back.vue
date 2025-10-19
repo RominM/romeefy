cb395f75b352a789860182e4a3ca220311dd1f6c
RominM <rominmanogil.off@gmail.com>
2025-10-19 11:40:59 +0200

    style: fix responsive player top fixed

---
+++ components/global/go-back.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="go-back">
+    <h-icon :icon="ArrowLeft02Icon" color="#fff" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeft02Icon } from '@hugeicons/core-free-icons';
+</script>
+
+<style scoped lang="scss">
+.go-back {
+  background-color: color-alpha($dark-surface, 0.9);
+  padding: 5px;
+  border-radius: 50%;
+}
+</style>
