bbd752e9894841cd4b4373a372f83bd6063372ac
Romin Manogil <romin@valuemycar.fr>
2025-10-19 11:40:59 +0200

    style: fix responsive player top fixed

--- components/artist/player-top-fixed.vue
+++ components/artist/player-top-fixed.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="play-top-fixed" :style="{ backgroundColor: color }">
     <div :class="['play-top-fixed__content', isVisible ? '--is-visible':'']">
-      <player-button :track-id="trackId"/>
+      <player-button v-if="isDesktop" :track-id="trackId"/>
+      <go-back v-else class="play-top-fixed__content--back" />
       <h2 class="play-top-fixed__content__name">{{ name }}</h2>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useDevice } from '~/composables/device/useDevice'
+
 
 const props = defineProps({
   color: { type: String, required: true },
@@ -16,8 +19,10 @@ const props = defineProps({
   trackId: { type: Number, required: true }
 })
 
+const { isDesktop } = useDevice()
+
 const isVisible = computed(() => {
-  const fortyVhInPixels = window.innerHeight * 0.4
+  const fortyVhInPixels = isDesktop.value ? window.innerHeight * 0.7 : window.innerHeight * 0.2 
   return props.scrollTop >= fortyVhInPixels
 })
 </script>
@@ -42,6 +47,24 @@ const isVisible = computed(() => {
     }
     &__name {
       color: #fff;
+    }
+  }
+}
+
+@media screen and (max-width: 540px) {
+  .play-top-fixed {
+    padding: 10px;
+    &__content {
+      margin-top: 10px;
+      &--back {
+        position: absolute;
+        left: 5px;
+        bottom: 5px;
+      }
+      &__name {
+        margin: auto;
+        font-size: 14px;
+      }
     }
   }
 }
