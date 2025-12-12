3daa637abfc1abaa6c6c6bf7460814ca0d6ca824
RominM <rominmanogil.off@gmail.com>
2025-12-12 22:15:14 +0100

    dev: added 404

--- error.vue
+++ error.vue
@@ -1,23 +1,68 @@
 <template>
-  <div class='error'>
-    <h1>404</h1>
-    <p>Oups, la page que vous cherchez n'existe pas</p>
-    <nuxt-link to="/" label="Accueil">Accueil</nuxt-link>
+  <div class='error-page'>
+    <div class='error-page__content'>
+      <h1 class='error-page__content__title'>Romeefy 404</h1>
+      <h2 class='error-page__content__subtitle'>404s and heartbreaks</h2>
+      <p class='error-page__content__text'>We couldn't find the page you <br/> were looking for. Maybe return at our
+        <nuxt-link class="link" to="/" title="home">Home</nuxt-link>
+      </p>
+      <p class='error-page__content__bottom'>go listen to some music instead</p>
+    </div>
+    <img class='error-page__img' src="/img/vecteezy_record-player_36661391.svg" alt="tourne disque">
   </div>
 </template>
 
 <script setup lang='ts'>
+
    
 </script>
 
 <style lang='scss' scoped>
-  .error{
+  .error-page {
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-    background-color: $dark-background;
-    color: $light-text-primary;
+    gap: 10%;
+    background-color: #FFCDD2;
+    color: $dark-text-primary;
     height: 100dvh;
+    padding: 20px;
+    user-select: none;
+    &__content {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      color: #000;
+      &__title {
+        margin-bottom: 15px;
+        font-size: 2.5rem;
+      }
+      &__text {
+        font-size: 15px;
+        font-weight: 100;
+        .link {
+          text-decoration: underline;
+        }
+      }
+      &__bottom {
+        text-transform: uppercase;
+        font-weight: 600;
+      }
+    }
+    &__img {
+      width: 30%;
+      min-width: 360px;
+    }
+  }
+
+  @media screen and (max-width: 805px) {
+    .error-page {
+      flex-direction: column;
+      width: 100%;
+      &__img {
+        width: 70%;
+        min-width: 0;
+      }
+    }
   }
 </style>
--- pages/private/index.vue
+++ pages/private/index.vue
@@ -1,11 +1,53 @@
 <template>
     <div class='private-test'>
-      <tri-sphere />
     </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+const API_KEY = "sOcT9fGiuMr5OdFhdL1u18ZG944sUTiV";
+const CLASSIFICATIONS_URL = `https://app.ticketmaster.com/discovery/v2/classifications.json?apikey=${API_KEY}`;
+
+onMounted(async () => {
+  try {
+    const genres = await fetchAllGenres();
+    console.log("Ticketmaster genres:", genres);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+async function fetchAllGenres() {
+  const res = await fetch(CLASSIFICATIONS_URL);
+  if (!res.ok) throw new Error("Erreur lors de la récupération des classifications");
+
+  const data = await res.json();
+  console.log({data});
+  
+  const classifications = data._embedded?.classifications || [];
+
+  const genres: Array<{ segment: string; genre: string; id: string; subgenres: any[] }> = [];
+
+  classifications.forEach((c: any) => {
+    const segmentName = c.segment?.name || "Unknown";
+    (c._embedded?.genres || []).forEach((g: any) => {
+      genres.push({
+        segment: segmentName,
+        genre: g.name,
+        id: g.id,
+        subgenres: (g._embedded?.subgenres || []).map((sg: any) => ({
+          id: sg.id,
+          name: sg.name,
+        })),
+      });
+    });
+  });
+
+  return genres;
+}
 </script>
+
 
 <style lang='scss' scoped>
     .private-test{}
