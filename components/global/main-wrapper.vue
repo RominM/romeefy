b478e773ac6181824b0d6a82d0d285f98ff2d98c
RominM <rominmanogil.off@gmail.com>
2026-05-06 19:45:54 +0200

    style; hot fix

--- components/global/main-wrapper.vue
+++ components/global/main-wrapper.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class='main-wrapper'>
+  <div class="main-wrapper">
     <slot />
   </div>
 </template>
 
-<script setup lang='ts'>
-</script>
+<script setup lang="ts"></script>
 
-<style lang='scss' scoped>
-  .main-wrapper{
-    height: 100%;
-    width: 100%;
-    border-radius: 8px;
-    background: #141313;
-    background-color: $dark-surface-secondary;
-  }
+<style lang="scss" scoped>
+.main-wrapper {
+  height: 100%;
+  width: 100%;
+  border-radius: 8px;
+  background: #141313;
+  background-color: $dark-surface-secondary;
+}
 </style>
